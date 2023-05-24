830f2c09dc16b4527c1556d09d3ede0f4a557291
Brian Matthews <brian@weareindustrial.com>
2023-05-24 10:28:30 -0500

    Update tests to cover all utms

--- cypress/e2e/visit.with.utms.cy.js
+++ cypress/e2e/visit.with.utms.cy.js
@@ -1,7 +1,7 @@
 describe("Visit WITH UTM", function() {
     
     it("stores utm data for utm visits ", function() {
-        cy.visit('http://localhost:8080/test/test-site/index.html?utm_source=test_source2&utm_campaign=test_campaign2&utm_medium=test_medium2');
+        cy.visit('http://localhost:8080/test/test-site/index.html?utm_source=test_source2&utm_campaign=test_campaign2&utm_medium=test_medium2&utm_content=test_content2&utm_term=test_term2');
         
         cy.getCookie('ils.utm_source')
         .should('have.property', 'value', 'test_source2');
@@ -11,11 +11,18 @@ describe("Visit WITH UTM", function() {
         
         cy.getCookie('ils.utm_medium')
         .should('have.property', 'value', 'test_medium2');
+
+        cy.getCookie('ils.utm_content')
+            .should('have.property', 'value', 'test_content2');
+
+        cy.getCookie('ils.utm_term')
+            .should('have.property', 'value', 'test_term2');
+
         
     });
 
-    it("maintians utm data for utm visits when navigating pages", function() {
-        cy.visit('http://localhost:8080/test/test-site/index.html?utm_source=test_source2&utm_campaign=test_campaign2&utm_medium=test_medium2');
+    it("maintian utm data for utm visits when navigating pages", function() {
+        cy.visit('http://localhost:8080/test/test-site/index.html?utm_source=test_source2&utm_campaign=test_campaign2&utm_medium=test_medium2&utm_content=test_content2&utm_term=test_term2');
         cy.get('#test-page-link').click();
 
         cy.getCookie('ils.utm_source')
@@ -26,6 +33,12 @@ describe("Visit WITH UTM", function() {
         
         cy.getCookie('ils.utm_medium')
         .should('have.property', 'value', 'test_medium2')
+
+        cy.getCookie('ils.utm_content')
+            .should('have.property', 'value', 'test_content2');
+
+        cy.getCookie('ils.utm_term')
+            .should('have.property', 'value', 'test_term2');
         
         
         cy.get('#home-page-link').click();
@@ -38,6 +51,12 @@ describe("Visit WITH UTM", function() {
         
         cy.getCookie('ils.utm_medium')
         .should('have.property', 'value', 'test_medium2')
+
+        cy.getCookie('ils.utm_content')
+            .should('have.property', 'value', 'test_content2');
+
+        cy.getCookie('ils.utm_term')
+            .should('have.property', 'value', 'test_term2');
         
     });
 });
